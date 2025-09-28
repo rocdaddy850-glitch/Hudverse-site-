0588af93d5f78742b36b339a1af031d0a5adab60
rocdaddy850-glitch <rocdaddy850@gmail.com>
2025-09-28 08:09:18 -0500

    Update supabaseClient.ts

--- Lib/supabaseClient.ts
+++ Lib/supabaseClient.ts
@@ -10,3 +10,37 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+let package = Package(
+    ...
+    dependencies: [
+        ...
+        .package(
+            url: "https://github.com/supabase/supabase-swift.git",
+            from: "2.0.0"
+        ),
+    ],
+    targets: [
+        .target(
+            name: "YourTargetName",
+            dependencies: [
+                .product(name: "Supabase", package: "supabase-swift") // Add as a dependency
+            ]
+        )
+    ]
+)let client = SupabaseClient(
+    supabaseURL: URL(string: "https://xyzcompany.supabase.co")!, 
+    supabaseKey: "public-anon-key",
+    options: SupabaseClientOptions(
+        db: .init(
+            schema: "public"
+        ),
+        auth: .init(
+            storage: MyCustomLocalStorage(),
+            flowType: .pkce
+        ),
+        global: .init(
+            headers: ["x-my-custom-header": "my-app-name"],
+            session: URLSession.myCustomSession
+        )
+    )
+)
